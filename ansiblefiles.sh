b0598fbb922f0476bcbac20188a643abd7ad0bb7
Andrejs Cunskis <acunskis@evolutiongaming.com>
2017-06-25 01:37:29 +0300

    Software installation to ansible, script updates

--- ansiblefiles.sh
+++ ansiblefiles.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ $# -gt 0 ]; then
-  ansible-playbook playbook.yml --tags "$1"
+  ansible-playbook playbook.yml --tags "$1" --ask-become-pass
 else
-  ansible-playbook playbook.yml
+  ansible-playbook playbook.yml --ask-become-pass
 fi
--- scripts/macos.sh
+++ scripts/macos.sh
@@ -9,9 +9,6 @@ osascript -e 'tell application "System Preferences" to quit'
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
@@ -170,21 +167,7 @@ defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
 ###############################################################################
-# Kill affected applications                                                  #
+# Finish                                                                      #
 ###############################################################################
 
-for app in "Activity Monitor" \
-	"Address Book" \
-	"Calendar" \
-	"cfprefsd" \
-	"Contacts" \
-	"Dock" \
-	"Finder" \
-	"Photos" \
-	"Safari" \
-	"Spectacle" \
-	"SystemUIServer" \
-	"Terminal"; do
-	killall "${app}" &> /dev/null
-done
-echo "Done. Note that some of these changes require a logout/restart to take effect."
+echo "Done. Note that some of these changes require application restart or logout/restart to take effect."
