1d80163ede71b25a7e387fae9d7c9401058f6d28
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2017-09-30 16:46:29 +0300

    Updates to shell and vim

--- ansiblefiles.sh
+++ ansiblefiles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -gt 0 ]; then
-  ansible-playbook playbook.yml --tags "$1" --ask-become-pass
+  ansible-playbook playbook.yml --tags "$1"
 else
-  ansible-playbook playbook.yml --ask-become-pass
+  ansible-playbook playbook.yml
 fi
