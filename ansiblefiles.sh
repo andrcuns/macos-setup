04554ff94c31fbaea173431adc1b3531894c4cf6
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2021-01-10 23:00:27 +0200

    Move sw instal to brewfile

--- ansiblefiles.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# -gt 0 ]; then
-  ansible-playbook playbook.yml --tags "$1"
-else
-  ansible-playbook playbook.yml
-fi
--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install ansible
-brew install ansible
+# Install brew software
+brew bundle
+
+# Run ansible tasks
+ansible-playbook playbook.yml
