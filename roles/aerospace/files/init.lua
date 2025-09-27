828a39c7d1c9e0529cb38ac9c5a0e95a0ebb81c5
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2025-09-27 11:23:06 +0300

    Adjust terminal size and position

--- roles/aerospace/files/init.lua
+++ roles/aerospace/files/init.lua
@@ -10,10 +10,10 @@ function placeTerminal(id)
     local screen = win:screen():frame()
 
     -- Desired proportions
-    local ww = math.floor(screen.w * 0.95)
-    local wh = math.floor(screen.h * 0.60)
+    local ww = math.floor(screen.w * 0.99)
+    local wh = math.floor(screen.h * 0.70)
     local x  = math.floor(screen.x + (screen.w - ww) / 2) -- center horizontally
-    local y  = math.floor(screen.y + (screen.h * 0.02))
+    local y  = math.floor(screen.y + (screen.h * 0.009))
 
     win:setFrame({x = x, y = y, w = ww, h = wh})
     win:focus()
