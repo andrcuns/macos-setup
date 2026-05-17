95bbe0feade318876357790179a4ac4c04ac52d8
andrejs <git@andrcuns.mozmail.com>
2026-05-17 10:26:40 +0300

    Add software update setup

---
+++ roles/zshell/files/utils.zsh
@@ -0,0 +1,15 @@
+function log() {
+  echo -e "\033[1;33m$1\033[0m"
+}
+
+function success() {
+  echo -e "\033[1;32m$1\033[0m"
+}
+
+function dotfiles() {
+  cd $(chezmoi source-path)
+}
+
+function dockerbash() {
+  docker exec -it $1 bash -l
+}
